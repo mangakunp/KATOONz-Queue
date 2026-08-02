78abc589884c259d11768306964aaa911dc84f6a
mangakunp <mangakunp@gmail.com>
2026-08-02 14:43:09 +0700

    up to v4.0

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 // วาง Firebase configuration ของคุณแทนค่าด้านล่าง
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.KTQM_FIREBASE_CONFIG = {
   apiKey: "AIzaSyBJBvHsDYZKMQzuv8IqIE9xBxg2MeYvE4g",
   authDomain: "katoonz-tomo-queue.firebaseapp.com",
+  databaseURL:"https://katoonz-tomo-queue-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "katoonz-tomo-queue",
   storageBucket: "katoonz-tomo-queue.firebasestorage.app",
   messagingSenderId: "516609889667",
